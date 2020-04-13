40cbcc1de7aa9c6e52397f9449b7ffc1765fcde5
Iannikan <imniklason@gmail.com>
2020-04-13 12:34:46 -0600

    moved Links out of App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
+import Navigation from './components/Navigation.jsx';
 import OfferList from './components/OfferList.jsx';
 import VolunteerList from './components/VolunteerList.jsx';
 import PatientList from './components/PatientList.jsx';
@@ -13,31 +13,12 @@ import Signin from './components/Signin.jsx';
 import Signup from './components/Signup.jsx';
 import theme from './theme';
 
-const StyledNav = styled.nav`
-color: ${(props) => props.theme.colors.main};
-background-color: ${(props) => props.theme.colors.secondary};
-display: flex;
-justify-content: space-around;
-padding 1em;
-margin-bottom: 1em;
-`;
-const StyledLink = styled(Link)`
-color: ${(props) => props.theme.colors.main};
-`;
-
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
           <Router>
-            <StyledNav>
-              <StyledLink to='/offers'>Offers</StyledLink>
-              <StyledLink to='/volunteers'>Volunteers</StyledLink>
-              <StyledLink to='/patients'>Patients</StyledLink>
-              {!('token' in sessionStorage) && <StyledLink to='/signup'>Sign Up</StyledLink>}
-              {!('token' in sessionStorage) && <StyledLink to='/signin'>Sign in</StyledLink>}
-              {'token' in sessionStorage && <StyledLink to='/signup'>My Profile</StyledLink>}
-            </StyledNav>
+            <Navigation/>
             <Switch>
               <Route path="/offers">
                 <OfferList/>
@@ -60,5 +41,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
---
+++ ui/src/components/Navigation.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  Link,
+} from 'react-router-dom';
+
+const StyledNav = styled.nav`
+color: ${(props) => props.theme.colors.main};
+background-color: ${(props) => props.theme.colors.secondary};
+display: flex;
+justify-content: space-around;
+padding 1em;
+margin-bottom: 1em;
+`;
+
+const StyledLink = styled(Link)`
+color: ${(props) => props.theme.colors.main};
+`;
+
+const Navigation = () => (
+  <StyledNav>
+    <StyledLink to='/offers'>Offers</StyledLink>
+    <StyledLink to='/volunteers'>Volunteers</StyledLink>
+    <StyledLink to='/patients'>Patients</StyledLink>
+    {!('token' in sessionStorage) && <StyledLink to='/signup'>Sign Up</StyledLink>}
+    {!('token' in sessionStorage) && <StyledLink to='/signin'>Sign in</StyledLink>}
+    {'token' in sessionStorage && <StyledLink to='/signup'>My Profile</StyledLink>}
+  </StyledNav>
+);
+
+export default Navigation;
