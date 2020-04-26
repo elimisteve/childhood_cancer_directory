41afa3bfc7499ff4bb2d7d7566ab94104e16f586
Iannikan <imniklason@gmail.com>
2020-04-26 14:07:32 -0600

    increased allowable length of user description

--- api/migrations/20200414194011-create-user.js
+++ api/migrations/20200414194011-create-user.js
@@ -22,7 +22,7 @@ module.exports = {
         allowNull: false,
       },
       description: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(4096),
         allowNull: false,
       },
       password: {
