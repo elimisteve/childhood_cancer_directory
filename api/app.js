3731afe553efb96d2a2a36095aa9e1860c124cc7
Iannikan <imniklason@gmail.com>
2020-04-27 13:25:04 -0600

    prepended api to api routes

--- api/app.js
+++ api/app.js
@@ -20,12 +20,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 
-app.use(offerRouter);
-app.use(userRouter);
-app.use(helpRouter);
+
+app.use('/api', offerRouter);
+app.use('/api', userRouter);
+app.use('/api', helpRouter);
 
 
 
--- ui/src/api.js
+++ ui/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000';
+const apiUrl = 'http://localhost:3000/api/';
 
 let token = null;
 const setToken = (newToken) => {
