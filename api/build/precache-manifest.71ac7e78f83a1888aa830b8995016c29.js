e36998007282fe1949219639b966139d1a271e97
Iannikan <imniklason@gmail.com>
2020-04-27 18:24:39 -0600

    build folder added to api

---
+++ api/build/precache-manifest.71ac7e78f83a1888aa830b8995016c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef2ad61127f3db351bf5ae79d0664f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f93a45eb4fdc646b172",
+    "url": "/static/js/2.83d64b18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83d64b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e545603a7450dd5394",
+    "url": "/static/js/main.8bc45845.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  },
+  {
+    "revision": "81645415ce2f842cd291d9dc7a913f5d",
+    "url": "/static/media/HandsPurple.81645415.png"
+  }
+]);
