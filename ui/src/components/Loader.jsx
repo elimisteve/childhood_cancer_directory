656062d27ef6d6e63615a4439c71e5188c9c648b
Iannikan <imniklason@gmail.com>
2020-04-29 14:39:18 -0600

    trying rotating purple hands for loader

--- ui/src/components/Loader.jsx
+++ ui/src/components/Loader.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import HandsPurple from '../assets/HandsPurple.png';
+
+const rotate = keyframes`
+from{
+  transform: rotate(0deg);
+}
+
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const Container = styled.div`
+width: 100%;
+height: 100%;
+text-align: center;
+`;
+
+const Rotate = styled.div`
+ display: inline-block;
+ animation: ${rotate} 2s linear infinite;
+ padding: 2rem 1rem;
+`;
+
+const StyledImg = styled.img`
+height: 50%;
+width: auto;
+`;
+
 
 const Loader = () => (
-  <div>Loading...</div>
+  <Container>
+    <Rotate><StyledImg src={HandsPurple} /> </Rotate>
+  </Container>
 );
 
 export default Loader;
