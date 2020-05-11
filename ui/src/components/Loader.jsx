9d19c1c2d4c6e97d42988f6142996aa022319be0
Iannikan <imniklason@gmail.com>
2020-05-11 14:28:14 -0600

    centered loader vertically

--- ui/src/components/Loader.jsx
+++ ui/src/components/Loader.jsx
@@ -13,9 +13,9 @@ to {
 `;
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
+position: absolute;
+top: 50%;
+margin: auto;
 width: 100%;
 height: 100%;
 text-align: center;
